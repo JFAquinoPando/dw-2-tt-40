477fd48888abfead3121784eeb417f7d61278797
JFAquinoPando <pandolcik@gmail.com>
2023-12-02 15:49:48 -0300

    test

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>Prueba desde mi maquina</h2>
+
 <?php
 var_dump(10.01);
 var_dump(
@@ -37,4 +47,8 @@ echo "<br>";
 $x = "23465.768";
 $int_cast = (int)$x;
 echo $int_cast;
+
+?>    
+</body>
+</html>
 
